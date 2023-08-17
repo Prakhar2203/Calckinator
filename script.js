eee65b65e227cf7bb39a9b56132e19d167cf1918
Prakhar2203 <123736967+Prakhar2203@users.noreply.github.com>
2023-08-18 05:28:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let string = "";
+let ctr=0;
+let temp="";
+
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == 'X'){
+      string=string+'*'
+      document.querySelector('input').value=string;
+    }
+    else if(e.target.innerHTML == '='){
+      string=eval(string)
+      document.querySelector('input').value=string;
+    }
+    else if(e.target.innerHTML == '%'){
+      string=string+'/'+100+'*';
+      document.querySelector('input').value=string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string=""
+      document.querySelector('input').value=string;
+    }
+    else{
+      document.querySelector('input').value=temp;
+      temp="";
+      ctr=0;
+      console.log(e.target)
+      string=string+e.target.innerHTML;
+      document.querySelector('input').value=string;
+    }
+  })
+})
